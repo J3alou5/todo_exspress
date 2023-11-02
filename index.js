69055af075513d4ec0aa1f25fbdcc1e01b132160
Karlis.Kade <karlis.kade@voco.ee>
2023-11-02 21:55:31 +0200

    write data to file

--- index.js
+++ index.js
@@ -6,18 +6,43 @@ const path = require("path")
 app.set("view engine", "ejs")
 app.set("views", path.join (__dirname, "views"))
 
-app.get("/", (req, res) => {
+const readFile = (filename) => {
+    return new Promise((resolve, _reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const tasks = data.split("\n")
+            resolve(tasks)
+        });
+    })
+}
 
-	fs.readFile("./tasks", "utf8", (err, data) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-		const tasks = data.split ("\n")
-	res.render("index", {tasks: tasks})
-	});
+app.get("/", (_req, res) => {
+    readFile("./tasks")
+       .then(tasks => {
+        console.log(tasks)
+        res.render("index", {tasks: tasks})
+    });
+})
+app.use(express.urlencoded({ extended: true}));
+
+app.post("/", (req, res) => {
+    readFile("./tasks")
+        .then(tasks => {
+        tasks.push(req.body.task)
+        const data = tasks.join("\n")
+        fs.writeFile("./tasks", data, err => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        res.redirect("/")
+        })
+    })
 })
 
 app.listen(3001, () => {
-	console.log("Example app is started at http://localhost:3001")
+    console.log("Example app is started at http://localhost:3001")
 })
