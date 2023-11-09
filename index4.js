a54fb192276d2fe8f89bbd8961150fe0bf9d37aa
Karlis.Kade <karlis.kade@voco.ee>
2023-11-09 22:10:59 +0200

    Kontrolltoo

--- index4.js
+++ index4.js
@@ -2,19 +2,15 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, 'utf-8', (err, data) => {
       if (err) {
         console.error(err);
-        reject(err);
         return;
       }
       const tasks = JSON.parse(data);
@@ -28,7 +24,6 @@ const writeFile = (filename, data) => {
     fs.writeFile(filename, data, 'utf-8', (err) => {
       if (err) {
         console.error(err);
-        reject(err);
         return;
       }
       resolve(true);
@@ -44,6 +39,8 @@ app.get('/', (req, res) => {
     });
   });
 });
+
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
   let error = null;
@@ -79,7 +76,11 @@ app.post('/', (req, res) => {
 app.get('/delete-task/:taskId', (req, res) => {
   const deletedTaskId = parseInt(req.params.taskId);
   readFile('./tasks.json').then((tasks) => {
-    tasks = tasks.filter((task) => task.id !== deletedTaskId);
+    tasks.forEach((task, index) => {
+      if (task.id === deletedTaskId) {
+        tasks.splice(index, 1);
+      }
+    });
     const data = JSON.stringify(tasks, null, 2);
     writeFile('tasks.json', data).then(() => {
       res.redirect('/');
@@ -96,35 +97,25 @@ app.get('/edit-task/:taskId', (req, res) => {
     } else {
       res.render('edit', {
         task: taskToEdit,
-        error: null,
       });
     }
   });
 });
 
 app.post('/update-task/:taskId', (req, res) => {
-  const updatedTaskId = parseInt(req.params.taskId);
-  const updatedTaskText = req.body.updatedTask.trim();
-
-  if (updatedTaskText.length === 0) {
-    res.render('edit', {
-      task: { id: updatedTaskId, task: updatedTaskText },
-      error: 'Please insert correct task data',
-    });
-  } else {
-    readFile('./tasks.json').then((tasks) => {
-      const taskToEdit = tasks.find((task) => task.id === updatedTaskId);
-      if (!taskToEdit) {
+  const editTaskId = parseInt(req.params.taskId);
+  readFile('./tasks.json').then((tasks) => {
+    const taskToEdit = tasks.find((task) => task.id === editTaskId);
+    if (!taskToEdit) {
+      res.redirect('/');
+    } else {
+      taskToEdit.task = req.body.updatedTask;
+      const data = JSON.stringify(tasks, null, 2);
+      writeFile('tasks.json', data).then(() => {
         res.redirect('/');
-      } else {
-        taskToEdit.task = updatedTaskText;
-        const data = JSON.stringify(tasks, null, 2);
-        writeFile('tasks.json', data).then(() => {
-          res.redirect('/');
-        });
-      }
-    });
-  }
+      });
+    }
+  });
 });
 
 app.post('/clear-all', (req, res) => {
